635a9744677f6e83d8e279adac82a2581ff00e0d
Anja Tihić <atihic99@gmail.com>
2024-03-27 15:31:31 +0100

    Add seekBar function example

--- app/src/main/java/hr/tvz/android/kalkulatortihic/MainActivity.kt
+++ app/src/main/java/hr/tvz/android/kalkulatortihic/MainActivity.kt
@@ -1,6 +1,8 @@
 package hr.tvz.android.kalkulatortihic
 
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+        //seekBar listener
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Toast.makeText(this@MainActivity, "Hello $progress", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // This method is called when the user starts tracking touch on the seek bar
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // This method is called when the user stops tracking touch on the seek bar
+            }
+        })
+
     }
 }
