1938ae3d1f2ba8ecdc2a359351de2bd6fe6d8be3
Anja Tihić <atihic99@gmail.com>
2024-03-27 16:05:48 +0100

    Add actual functionality

--- app/src/main/java/hr/tvz/android/kalkulatortihic/MainActivity.kt
+++ app/src/main/java/hr/tvz/android/kalkulatortihic/MainActivity.kt
@@ -1,12 +1,18 @@
 package hr.tvz.android.kalkulatortihic
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.SeekBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +41,34 @@ class MainActivity : AppCompatActivity() {
                 // This method is called when the user stops tracking touch on the seek bar
             }
         })
+
+    }
+
+    fun calculateDiscountedP(view: View){
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
+        val resultViewer = findViewById<TextView>(R.id.textViewCalculationResult)
+        val resultTitle: TextView = findViewById(R.id.textViewResult)
+
+        val oPriceStr = findViewById<EditText>(R.id.origPriceInput).text.toString()
+        val percentStr = findViewById<EditText>(R.id.percentInput).text.toString()
+
+        if(oPriceStr.isNotEmpty() && percentStr.isNotEmpty()){
+            val oPrice = oPriceStr.toDouble()
+            val percent = percentStr.toDouble()
+
+            val result = oPrice - (oPrice * (percent/100))
+            val decimalFormat = DecimalFormat("#.00")
+
+            resultTitle.visibility = TextView.VISIBLE
+            resultViewer.text = buildString {
+                append(decimalFormat.format(result))
+                append(" €")
+            }
+        }else{
+            Toast.makeText(this@MainActivity, "Please input both numbers", Toast.LENGTH_LONG).show()
+        }
 
     }
 }
