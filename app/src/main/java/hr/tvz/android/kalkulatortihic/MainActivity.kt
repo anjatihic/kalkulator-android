0c445d1d179827447d42cf1b359abfb90103d130
Anja Tihić <atihic99@gmail.com>
2024-03-28 09:54:13 +0100

    Add view binding

--- app/src/main/java/hr/tvz/android/kalkulatortihic/MainActivity.kt
+++ app/src/main/java/hr/tvz/android/kalkulatortihic/MainActivity.kt
@@ -4,38 +4,47 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
+import hr.tvz.android.kalkulatortihic.databinding.ActivityMainBinding
 import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        //view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        binding.calculateButton.setOnClickListener { view ->
+            calculateDiscountedPrice(view)
+        }
+
     }
 
-    fun calculateDiscountedP(view: View){
+    private fun calculateDiscountedPrice(view: View){
         //close keyboard
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
 
-        val resultViewer = findViewById<TextView>(R.id.textViewCalculationResult)
-        val resultTitle: TextView = findViewById(R.id.textViewResult)
+        val resultViewer = binding.textViewCalculationResult
+        val resultTitle = binding.textViewResult
 
-        val oPriceStr = findViewById<EditText>(R.id.origPriceInput).text.toString()
-        val percentStr = findViewById<EditText>(R.id.percentInput).text.toString()
+        val oPriceStr = binding.origPriceInput.text.toString()
+        val percentStr = binding.percentInput.text.toString()
 
         if(oPriceStr.isNotEmpty() && percentStr.isNotEmpty()){
             val oPrice = oPriceStr.toDouble()
@@ -50,7 +59,7 @@ class MainActivity : AppCompatActivity() {
                 append(" €")
             }
         }else{
-            Snackbar.make(view, "Unesite oba dva broja", Snackbar.LENGTH_LONG).show()
+            Snackbar.make(view, R.string.valid_message, Snackbar.LENGTH_LONG).show()
         }
 
     }
